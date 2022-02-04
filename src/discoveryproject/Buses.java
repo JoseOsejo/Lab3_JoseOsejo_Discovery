4a7a78a3dff0880f9d1004601f3d2daaa97cbb44
JoseOsejo <80367652+JoseOsejo@users.noreply.github.com>
2022-02-04 16:08:40 -0600

    Buse atributos mutadores

--- src/discoveryproject/Buses.java
+++ src/discoveryproject/Buses.java
@@ -11,7 +11,47 @@ import java.util.ArrayList;
  *
  * @author jcoq2
  */
-public class Buses extends Transporte
-{
+public class Buses extends Transporte {
+
     private ArrayList<Alumno> alumnos;
+    private int numeroSillas;
+    private int capacidadPersonasPie;
+    //el numero de sillas + numeroCapacidadPersonasPie = cantidad de Alumnos
+
+    public Buses(ArrayList<Alumno> alumnos, int numeroSillas, int capacidadPersonasPie, String placa, String color, Transportista transportista, ArrayList<Estacion> estaciones) {
+        super(placa, color, transportista, estaciones);
+        this.alumnos = alumnos;
+        this.numeroSillas = numeroSillas;
+        this.capacidadPersonasPie = capacidadPersonasPie;
+    }
+
+    public ArrayList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public int getNumeroSillas() {
+        return numeroSillas;
+    }
+
+    public void setNumeroSillas(int numeroSillas) {
+        this.numeroSillas = numeroSillas;
+    }
+
+    public int getCapacidadPersonasPie() {
+        return capacidadPersonasPie;
+    }
+
+    public void setCapacidadPersonasPie(int capacidadPersonasPie) {
+        this.capacidadPersonasPie = capacidadPersonasPie;
+    }
+
+    @Override
+    public String toString() {
+        return "Buses{" + "alumnos=" + alumnos + ", numeroSillas=" + numeroSillas + ", capacidadPersonasPie=" + capacidadPersonasPie + '}';
+    }
+
 }
